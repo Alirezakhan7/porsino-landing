4852fd2cc07c33fcb3e1b0b3d6f9c464201d0dd4
AlirezaKhan7 <note4.alireza@gmail.com>
2025-05-01 14:41:16 +0330

    آپدیت جدید پروژه

--- components/home-content.tsx
+++ components/home-content.tsx
@@ -424,7 +424,7 @@ export default function HomeContent() {
             </div>
 
             {/* Key Achievements */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 text-gray-800">
               {achievements.map((achievement, index) => (
                 <motion.div
                   key={index}
