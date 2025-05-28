2ad21edec31ecb76f9e4707cfd0eb9d9212b35b1
AlirezaKhan7 <note4.alireza@gmail.com>
2025-05-28 12:46:40 +0330

    توضیح درباره تغییرات

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 // next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://porsino.ir', // آدرس اصلی دامنه‌ت (با https!)
+  siteUrl: 'https://porsino.org', // آدرس اصلی دامنه‌ت (با https!)
   generateRobotsTxt: true,       // فایل robots.txt هم تولید کنه
   sitemapSize: 5000,             // تعداد لینک در هر sitemap
   changefreq: 'daily',
